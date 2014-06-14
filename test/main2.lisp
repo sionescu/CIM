5321543814faf9f5471975b199a74bb6d34a317a
Masataro Asai <guicho2.71828@gmail.com>
2014-06-14 21:57:59 +0900

    Testing: regenerate ql_dep error

--- test/main2.lisp
+++ test/main2.lisp
@@ -3,3 +3,10 @@
 
 ;; TODO: -c, -d, -i, -l, -L, -r, -q, -Q, --no-rl, --no-right, --no-color, --help
 
+(test ccl-ql-dep-error
+  (finishes
+    (fresh-main `("--no-init"
+                  "-C" ,(cim_home "shell-test/")
+                  "--"
+                  "cimtest/scripts/ql_cmd_deps"
+                  "--path" "cimtest/quicklisp/"))))
