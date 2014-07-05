d4f5cc7fddd7703d11545e2560f53f3481e4a47c
Masataro Asai <guicho2.71828@gmail.com>
2014-07-05 22:58:37 +0900

    refined build.sh

--- lib/build.sh
+++ lib/build.sh
@@ -1,19 +1,27 @@
 #!/bin/sh
 
-sources=$(echo packages.lisp no-debug.lisp cim.lisp option-parser.lisp repl.lisp process-args.lisp mains.lisp run.lisp)
+sources="packages.lisp no-debug.lisp cim.lisp option-parser.lisp repl.lisp process-args.lisp mains.lisp run.lisp"
+
+run(){
+    cat > script.lisp <<EOF
+;; This is an automatically generated lisp file.
+;; Modification to this file has no effect on the main source
+;; and may be overwritten by the later call to the build.sh .
+EOF
+    cat $sources >> script.lisp
+    exit 0
+}
 
 if [ ! -e script.lisp ]
 then
-    cat $sources > script.lisp
-    exit 0
+    run
 fi
 
 for lisp in $sources
 do
     if [ $lisp -nt script.lisp ]
     then
-        cat $sources > script.lisp
-        exit 0
+        run
     fi
 done
 
