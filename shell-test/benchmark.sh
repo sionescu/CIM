12a62726e729782f83fc8b81132dcc91615ce3dd
Masataro Asai <guicho2.71828@gmail.com>
2014-07-06 20:08:30 +0900

    benchmark: If script.lisp was compiled with -c option, add a execution bit and use it. This results in a super-speedup!

--- shell-test/benchmark.sh
+++ shell-test/benchmark.sh
@@ -1,8 +1,41 @@
 #!/bin/bash
 
+source .bashrc
+export HOME=$(readlink -f ./)
+export CIM_HOME=cimtest/
+
+echorun(){
+    echo $@
+    $@
+}
+
+echorun rm -f cimtest/lib/script*
+echorun rm -f ../lib/script*
+echorun make all
+
+echo total runtime of 5 runs:
+echo w/ quicklisp
 time {
 for i in {1..5}
 do
-    time cl -e "(print $i)"
+    cl -e "(print $i)"
+done
+}
+
+echo w/o quicklisp with --no-init option
+time {
+for i in {1..5}
+do
+    cl --no-init -e "(print $i)"
+done
+}
+
+echo w/o quicklisp, script.lisp compiled
+echorun cl --no-init -c "cimtest/lib/script.lisp" -Q
+
+time {
+for i in {1..5}
+do
+    cl --no-init -e "(print $i)"
 done
 }
