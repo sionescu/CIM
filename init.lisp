5497fcf44d88ea7abb37512ab68fcfd1f69e3fb7
Masataro Asai <guicho2.71828@gmail.com>
2014-07-06 15:26:30 +0900

    Fixed: ql_home -> cim:ql_home

--- init.lisp
+++ init.lisp
@@ -1,7 +1,7 @@
 
 ;;; The following lines added by ql:add-to-init-file:
 #-quicklisp
-(let ((quicklisp-init (ql_home "/setup.lisp")))
+(let ((quicklisp-init (cim:ql_home "/setup.lisp")))
   (when (probe-file quicklisp-init)
     (load quicklisp-init :verbose nil)))
 
