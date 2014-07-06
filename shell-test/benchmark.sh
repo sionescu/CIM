fd620355878b91670e01da8ea7a1ca6fc04ca2f1
Masataro Asai <guicho2.71828@gmail.com>
2014-07-06 21:19:07 +0900

    banchmark.sh: don't run with quicklisp

--- shell-test/benchmark.sh
+++ shell-test/benchmark.sh
@@ -4,6 +4,7 @@ source .bashrc
 export HOME=$(readlink -f $(dirname $0))
 # export CIM_HOME=cimtest/
 
+cim use ${1:-sbcl-system}
 cim info
 
 echorun(){
@@ -20,13 +21,13 @@ echorun rm -f ../lib/script*
 echorun make all
 
 echo total runtime of 5 runs:
-echo w/ quicklisp
-time {
-for i in {1..5}
-do
-    echocl -e "(print $i)"
-done
-}
+# echo w/ quicklisp
+# time {
+# for i in {1..5}
+# do
+#     echocl -e "(print $i)"
+# done
+# }
 
 echo w/o quicklisp with --no-init option
 time {
