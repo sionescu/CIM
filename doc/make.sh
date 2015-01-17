9e1edb9f139f9830924da83778765a596b2803c9
Sunrin SHIMURA (keen) <3han5chou7@gmail.com>
2015-01-17 16:35:04 +0000

    build for man pages

---
+++ doc/make.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+DOC_ROOT="$(cd $(dirname $0); pwd)"
+MAN_DIR="${DOC_ROOT}/man/man1"
+
+txt2man(){
+    if [ "$1" -nt "${MAN_DIR}/$(basename $1 .txt).1" ]; then
+        a2x -v  --doctype manpage --format manpage -D "${MAN_DIR}" "$1" > "log/$(basename $1 .txt).log" 2>&1 ||
+            echo "Bulid failed: $1. See log/$(basename $1 .txt).log"
+    fi
+}
+
+cd "${DOC_ROOT}"
+for f in *.txt; do
+    txt2man "$f" &
+done
+
+wait
