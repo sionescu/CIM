33b39f87a2af5f979f22d31a7391630e6caaf209
Sunrin SHIMURA (keen) <3han5chou7@gmail.com>
2015-01-21 10:27:17 +0000

    Also build man when the target file doesnt exist

--- doc/make.sh
+++ doc/make.sh
@@ -4,7 +4,8 @@ DOC_ROOT="$(cd $(dirname $0); pwd)"
 MAN_DIR="${DOC_ROOT}/man/man1"
 
 txt2man(){
-    if [ "$1" -nt "${MAN_DIR}/$(basename $1 .txt).1" ]; then
+    if [ ! -e "${MAN_DIR}/$(basename $1 .txt).1" ] || [ "$1" -nt "${MAN_DIR}/$(basename $1 .txt).1" ]; then
+        echo "Building $1"
         a2x -v  --doctype manpage --format manpage -D "${MAN_DIR}" "$1" > "log/$(basename $1 .txt).log" 2>&1 ||
             echo "Bulid failed: $1. See log/$(basename $1 .txt).log"
     fi
